79f47a8eb6d5dfeb370793d244d84acc53a579c6
Abiskar Acharya <acharya.abiskar7@gmail.com>
2026-03-20 17:01:08 +0545

    chore: final production security polish and babel reanimated fix

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,15 @@
 module.exports = function (api) {
   api.cache(true);
   const plugins = [
-    'react-native-paper/babel',
-    'react-native-reanimated/plugin',
+    'react-native-paper/babel'
   ];
 
   if (process.env.NODE_ENV === 'production') {
     plugins.push('transform-remove-console');
   }
+
+  // react-native-reanimated/plugin MUST be the absolute last plugin
+  plugins.push('react-native-reanimated/plugin');
 
   return {
     presets: ['babel-preset-expo'],
